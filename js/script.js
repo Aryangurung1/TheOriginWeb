777249512664baef1236152fe0a0b54c31a01ef8
Nirvic <rnirvic@gmail.com>
2023-07-17 11:55:14 +0545

    Nav redirection

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ let myAge = 13;
 let myDogRatio = 7;
 let myDogAge = myAge * myDogRatio;
 console.log(myDogAge);
+
+//Redirection
+
+const redirect = {
+    home: '/index.html',
+    about: '/html/about.html',
+    contact: '/html/contact.html',
+    service: '/html/service.html'
+}
+
+function redirectTo(page){
+    const url = redirect[page];
+    if(url){
+        window.location.href = url;
+    }
+}
+
